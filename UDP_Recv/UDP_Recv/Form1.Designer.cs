f13aa5cc43bb734a8f6a723e93c015818cf93e7a
skysaver00 <skysaver00@naver.com>
2021-07-27 13:20:07 +0900

    2021-07-27

--- UDP_Recv/UDP_Recv/Form1.Designer.cs
+++ UDP_Recv/UDP_Recv/Form1.Designer.cs
@@ -29,13 +29,50 @@ namespace UDP_Recv
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.colorDialog2 = new System.Windows.Forms.ColorDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("맑은 고딕", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(12, 118);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(194, 99);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "수신 시작";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(212, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(287, 318);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(511, 342);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.ColorDialog colorDialog2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
     }
 }
 
--- UDP_Recv/UDP_Recv/Form1.cs
+++ UDP_Recv/UDP_Recv/Form1.cs
@@ -7,15 +7,53 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
 
 namespace UDP_Recv
 {
     public partial class Form1 : Form
     {
+        UdpClient Client = new UdpClient(8080); //포트 번호
+        String data = "";
+
         public Form1()
         {
             InitializeComponent();
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Client.BeginReceive(new AsyncCallback(recv), null);
+            }
+            catch(Exception ex)
+            {
+                richTextBox1.Text += ex.Message.ToString();
+            }
+        }
+
+        void recv(IAsyncResult res)
+        {
+            IPEndPoint RemoteIP = new IPEndPoint(IPAddress.Any, 60240);
+            byte[] received = Client.EndReceive(res, ref RemoteIP);
+            data = Encoding.ASCII.GetString(received);
+
+            string hexData = "";
+            foreach (char each in data)
+            {
+                int value = Convert.ToInt32(each);
+                hexData += String.Format("{0:X}", value);
+                hexData += " ";
+            }
+            hexData += "\n";
+
+
+            this.Invoke(new MethodInvoker(delegate
+            {
+                richTextBox1.Text += hexData;
+            }));
+        }
     }
 }
