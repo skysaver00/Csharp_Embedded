6db045a68734cee14498206e96e7ecfac9d836ca
skysaver00 <skysaver00@naver.com>
2021-07-29 10:06:49 +0900

    2021-07-29

--- First_GUI/First_GUI/Form1.Designer.cs
+++ First_GUI/First_GUI/Form1.Designer.cs
@@ -31,16 +31,21 @@ namespace First_GUI
         {
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.Red;
-            this.button1.Location = new System.Drawing.Point(1831, 998);
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Font = new System.Drawing.Font("맑은 고딕", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.button1.Location = new System.Drawing.Point(1797, 982);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(77, 30);
+            this.button1.Size = new System.Drawing.Size(91, 35);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "종료";
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -49,11 +54,37 @@ namespace First_GUI
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("맑은 고딕", 29.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point);
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(862, 9);
+            this.label1.Location = new System.Drawing.Point(872, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(153, 52);
             this.label1.TabIndex = 1;
             this.label1.Text = "역 이름";
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.Font = new System.Drawing.Font("맑은 고딕", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.Blue;
+            this.label2.Location = new System.Drawing.Point(455, 625);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(405, 35);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "T2";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.White;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label3.Font = new System.Drawing.Font("맑은 고딕", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.Color.Blue;
+            this.label3.Location = new System.Drawing.Point(866, 625);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(405, 35);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "T2";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // stationMainForm
             // 
@@ -62,6 +93,8 @@ namespace First_GUI
             this.AutoSize = true;
             this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.ClientSize = new System.Drawing.Size(1920, 1040);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
@@ -79,6 +112,8 @@ namespace First_GUI
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
